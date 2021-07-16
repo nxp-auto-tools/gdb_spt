01460b486aa98a225ac600921b304ade0af0e75e
Stefan-mihai Moga <stefan-mihai.moga@nxp.com>
2021-07-16 08:25:25 +0300

    Changed copyright support based on Alexander Fedotov's comment.

--- opcodes/apex-dis.c
+++ opcodes/apex-dis.c
@@ -1,5 +1,5 @@
 /* Copyright (C) 1992-2021 Free Software Foundation, Inc.
-   Contributed by NXP, SPT support.
+   Contributed by NXP, APEX support.
 
    This file is part of BFD, the Binary File Descriptor library.
 
